a19c214329ada60ab31028eaf83ce720881387e8
ToraMullings316 <tora.mullings@stonybrook.edu>
2020-05-31 22:30:52 -0400

    Completed core functionality

--- Calculator_App/app/src/main/java/com/example/calculator_app/MainActivity.java
+++ Calculator_App/app/src/main/java/com/example/calculator_app/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.calculator_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.view.View.VISIBLE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,86 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initPlusButton();
+        initMinusButton();
+        initMultiplyButton();
+        initDivideButton();
+    }
+    private void initDivideButton() {
+        Button divideButton = findViewById(R.id.buttonDivide);
+        divideButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                EditText editFirstNum = findViewById(R.id.editTextFirstNum);
+                double firstNum = Double.parseDouble(editFirstNum.getEditableText().toString());
+                EditText editSecondNum = findViewById(R.id.editTextSecondNum);
+                double secondNum = Double.parseDouble(editSecondNum.getEditableText().toString());
+                double quotient = firstNum/secondNum;
+                String answer = "Answer: " + Math.round(quotient*100.0) / 100.0;
+                //Resources res = getResources();
+                //String answerString = res.getString(R.string.answer);
+                TextView ans = findViewById(R.id.textViewAnswer);
+                ans.setText(answer);
+            }
+        });
+    }
+    private void initMultiplyButton() {
+        Button multiplyButton = findViewById(R.id.buttonMultiply);
+        multiplyButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                EditText editFirstNum = findViewById(R.id.editTextFirstNum);
+                double firstNum = Double.parseDouble(editFirstNum.getEditableText().toString());
+                EditText editSecondNum = findViewById(R.id.editTextSecondNum);
+                double secondNum = Double.parseDouble(editSecondNum.getEditableText().toString());
+                double product = firstNum*secondNum;
+                String answer = "Answer: " + Math.round(product*100.0) / 100.0;
+                //Resources res = getResources();
+                //String answerString = res.getString(R.string.answer);
+                TextView ans = findViewById(R.id.textViewAnswer);
+                ans.setText(answer);
+            }
+        });
+    }
+    private void initMinusButton() {
+        Button minusButton = findViewById(R.id.buttonMinus);
+        minusButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                EditText editFirstNum = findViewById(R.id.editTextFirstNum);
+                double firstNum = Double.parseDouble(editFirstNum.getEditableText().toString());
+                EditText editSecondNum = findViewById(R.id.editTextSecondNum);
+                double secondNum = Double.parseDouble(editSecondNum.getEditableText().toString());
+                double difference = firstNum-secondNum;
+                String answer = "Answer: " + Math.round(difference*100.0) / 100.0;
+                //Resources res = getResources();
+                //String answerString = res.getString(R.string.answer);
+                TextView ans = findViewById(R.id.textViewAnswer);
+                ans.setText(answer);
+            }
+        });
+    }
+    private void initPlusButton() {
+        Button plusButton = findViewById(R.id.buttonPlus);
+        plusButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                EditText editFirstNum = findViewById(R.id.editTextFirstNum);
+                double firstNum = Double.parseDouble(editFirstNum.getEditableText().toString());
+                EditText editSecondNum = findViewById(R.id.editTextSecondNum);
+                double secondNum = Double.parseDouble(editSecondNum.getEditableText().toString());
+                //double roundOff = Math.round(a * 100.0) / 100.0;
+                double sum = firstNum+secondNum;
+                String answer = "Answer: " + Math.round(sum*100.0) / 100.0;
+                //Resources res = getResources();
+                //String answerString = res.getString(R.string.answer);
+                TextView ans = findViewById(R.id.textViewAnswer);
+                ans.setText(answer);
+            }
+        });
     }
 }
